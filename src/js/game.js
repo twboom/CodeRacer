51a8223062ade33ef408d951abe8dd0beee0c527
Thijs Boom <twboom@hotmail.nl>
2021-03-24 17:40:38 +0100

    Added terrible CSS

--- src/js/game.js
+++ src/js/game.js
@@ -2,16 +2,27 @@ game = [];
 game.game = document.getElementById('game-field');
 game.ctx = game.game.getContext('2d');
 
-game.frame = function() {
+game.size = function() {
     const ctx = game.ctx;
-    ctx.clearRect(0, 0, game.game.width, game.game.height)
+    ctx.canvas.width = window.innerWidth / 2.5;
+    ctx.canvas.height = window.innerHeight / 2.7;
+}
+
+game.frame = async function() {
+    const ctx = game.ctx;
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
 
     game.ctx.beginPath();
-    game.ctx.arc(Math.random() * 100, 50, 40, 0, 2 * Math.PI);
+    game.ctx.arc(50, 50, 40, 0, 2 * Math.PI);
     game.ctx.stroke();
 
     console.log('frame')
-    window.requestAnimationFrame(game.frame)
+    //requestAnimationFrame(game.frame)
 }
 
-game.frame()
+game.init = function() {
+    game.size()
+    game.frame()
+}
+
+game.init()
